b2903025990c3e7564a7c72051669a700f405dfa
Michael Harris <45339433+harrismichael2095@users.noreply.github.com>
2022-07-05 13:09:49 -0400

    Add files via upload

---
+++ site/src/View/Report/HtmlView.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace MichaelHarris\Component\studentstats\Site\View\Stats;
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+class HtmlView extends BaseHtmlView {
+    
+    public function display($template = null) {
+        // Call the parent display to display the layout file
+        parent::display($template);
+    }
+
+}
