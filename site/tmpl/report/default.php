a752e3f86a07c8373a3b9542ebc55baca9be3b36
Michael Harris <45339433+harrismichael2095@users.noreply.github.com>
2022-07-11 00:31:10 -0400

    Update default.php
    added query for reports

--- site/tmpl/report/default.php
+++ site/tmpl/report/default.php
@@ -1,13 +1,50 @@
 <?php
-
-defined('_JEXEC') or die('Restricted Access');
-
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Factory;
-use Joomla\CMS\Uri\Uri;
-use Joomla\CMS\Date\Date;
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_studentstats
+ *
+ * @copyright   Copyright (C) 2022 Michael Harris. All rights reserved.
+ * @license     GNU General Public License version 3; see LICENSE
+ */
+
+ // No direct access to this file
+defined('_JEXEC') or die('Restricted Access');
+?>
 
 
 
+
+<?php
+$db = JFactory::getDbo();
+$groupId = 11;
+$access = new JAccess();
+$members = $access->getUsersByGroup($groupId);
+$rows = '';
+$users = [];
+foreach ($members as $id) {
+    $user = JFactory::getUser($id);
+    array_push($users, $user);
+    $query = $db->getQuery(true);
+    $query->select('*');
+    $query->from('reports');
+    $query->where('id ='.$id);
+    $db->setQuery((string) $query);
+    $results = $db->loadAssocList();
+    $num_rows = count($results);
+    $query->select('*');
+    $query->from('reports');
+    $query->where('id!='.$id);
+    $results2 = $db->loadAssocList();
+    $missed_rows = count($results2);
+    $rows .= '<tr>';
+    $rows .= '<td>' . $user->name . '</td>';
+    $rows .= '<td>' . $num_rows . '</td>';
+    $rows .= '<td>' . $missed_rows . '</td>';
+    $rows .= '</tr>';
+}
 ?>
 
 <style>
@@ -26,4 +63,6 @@ use Joomla\CMS\Date\Date;
         <th>Missed Reports</th>
         <th>Percentage</th>
     </tr>
+    <?php echo $rows; ?>
 </table>
+
